3851a7b0c024a5433a0c5496feda6ee5947a3c01
MarlonFerro <correo@hotmail.com>
2020-07-22 19:39:17 -0500

    add changes to firts server

--- server.js
+++ server.js
@@ -1,6 +1,22 @@
 const express = require('express');
 const server = express();
 
+server.get('/me', (req,res) => {
+
+    res.send('Hola Mundo');
+
+})
+
+server.get('/me/:perros', (req,res) =>{
+
+    const names = ['Nico', 'Manuel', 'Joan'];
+    const id = req.params.perros;
+    res.send(names[id]);
+})
+
+
+
 server.listen(3000, ()=>{
     console.log('servidor listo');
 });
+
